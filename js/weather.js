7cf3428e33c7aaea6ae5f2ea350ef77e172914c6
Duck-le-bleu <80119929+Duck-le-bleu@users.noreply.github.com>
2021-03-13 18:47:13 -0500

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 38.682020;
+  let longitude = -87.512500;
 
   getWeather(latitude, longitude);
 }
